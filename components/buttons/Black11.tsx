ac0cb3966e046006c5da47f6d86e36bde908fde8
ibelick <julien.thibeaut@gmail.com>
2024-01-22 22:17:55 +0100

    feat: add isolate

--- components/buttons/Black11.tsx
+++ components/buttons/Black11.tsx
@@ -1,10 +1,8 @@
 export function Black11() {
   return (
-    <button
-      className="relative z-10 h-12 overflow-hidden rounded-md bg-neutral-950 px-8 py-2 
-        text-neutral-50 before:absolute before:left-0 before:-z-10 before:aspect-square before:w-full before:origin-center before:-translate-x-full before:rounded-full before:bg-blue-500 before:transition-all before:duration-500 before:hover:-translate-x-0 before:hover:scale-150"
-    >
-      Hover Me
+    <button className="group relative isolate z-10 h-12 overflow-hidden rounded-md bg-neutral-950 px-8 py-2 text-neutral-50">
+      <span>Hover Me</span>
+      <span className="absolute left-0 -z-10 aspect-square w-full origin-center -translate-x-full rounded-full bg-blue-500 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150" />
     </button>
   );
 }
--- components/buttons/BlackSm2.tsx
+++ components/buttons/BlackSm2.tsx
@@ -1,7 +1,7 @@
 export function BlackSm2() {
   return (
     <button className="cursor-pointer rounded-[8px] bg-neutral-950 px-3 py-1 text-sm text-neutral-100 transition-colors hover:bg-neutral-800 active:bg-neutral-700">
-      Click me
+      Hover me
     </button>
   );
 }
