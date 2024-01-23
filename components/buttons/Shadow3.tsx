c4ae86a4b704421129a48203c294d4e32e015991
ibelick <julien.thibeaut@gmail.com>
2024-01-23 11:07:17 +0100

    fix: shadow translate

--- components/buttons/Shadow3.tsx
+++ components/buttons/Shadow3.tsx
@@ -1,7 +1,10 @@
 export function Shadow3() {
   return (
-    <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 hover:-translate-x-3 hover:-translate-y-3 hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]">
-      Hover me
+    <button className="group relative">
+      <div className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">
+        Hover me
+      </div>
+      <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]"></div>
     </button>
   );
 }
--- components/buttons/Shadow4.tsx
+++ components/buttons/Shadow4.tsx
@@ -1,7 +1,10 @@
 export function Shadow4() {
   return (
-    <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 hover:-translate-x-3 hover:-translate-y-3 hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] active:translate-x-0 active:translate-y-0 active:shadow-none">
-      Click me
+    <button className="group relative">
+      <div className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-active:translate-x-0 group-active:translate-y-0">
+        Hover me
+      </div>
+      <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
     </button>
   );
 }
