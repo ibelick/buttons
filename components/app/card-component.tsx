bd89c7ecea6445225c75b6ce18cc8aadf34ac3c8
Maxime Tamburrini <maximemickael.contact@gmail.com>
2024-02-15 11:43:41 +0100

    save my work

--- components/app/card-component.tsx
+++ components/app/card-component.tsx
@@ -1,61 +1,68 @@
-"use client";
 import { CopyIcon, CheckIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { ReactInstance, useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
+import { TButton } from "@/data/buttons";
+import React from "react";
+import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import { flushSync } from "react-dom";
 
 type CardComponentProps = {
-  children: React.ReactNode;
+    ChildComponent: TButton;
 };
 
-export const CardComponent: React.FC<CardComponentProps> = ({ children }) => {
-  const [hasCheckIcon, setHasCheckIcon] = useState(false);
+export const CardComponent: React.FC<CardComponentProps> = ({
+    ChildComponent,
+}) => {
+    const [hasCheckIcon, setHasCheckIcon] = useState(false);
+    const parentRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if(parentRef.current) {
+            const parentRoot = createRoot(parentRef.current)
+            parentRoot.render(<ChildComponent/>)
 
-  const getCode = () => {
-    const div = document.createElement("div");
-    const root = createRoot(div);
-    flushSync(() => {
-      root.render(children);
-    });
+        }
 
-    const code = div.innerHTML;
-    return code;
-  };
+    }, [ChildComponent])
 
-  const onCopy = () => {
-    const code = getCode();
-    toast.success("Copied to clipboard!");
-    navigator.clipboard.writeText(code);
-    setHasCheckIcon(true);
+    const onCopy = () => {
+        const code = parentRef.current?.childNodes[0]!
+        if(!code) return
 
-    setTimeout(() => {
-      setHasCheckIcon(false);
-    }, 1000);
-  };
+        const htmlElement = ReactDOM.findDOMNode(code)! as Element
 
-  return (
-    <div className="relative flex items-center justify-center rounded-[4px] bg-neutral-50 px-0 py-24 md:px-2">
-      <div
-        className="absolute right-3 top-3 cursor-pointer bg-transparent p-2"
-        onClick={onCopy}
-      >
-        <div
-          className={`absolute inset-0  transform transition-all duration-300  ${
-            hasCheckIcon ? "scale-0 opacity-0" : "scale-100 opacity-100"
-          }`}
-        >
-          <CopyIcon className="text-neutral-600" />
+        if(!htmlElement) return 
+
+        const html = htmlElement.outerHTML
+        toast.success("Copied to clipboard!");
+        navigator.clipboard.writeText(html);
+        setHasCheckIcon(true);
+
+        setTimeout(() => {
+            setHasCheckIcon(false);
+        }, 1000);
+    };
+
+    return (
+        <div className="relative flex items-center justify-center rounded-[4px] bg-neutral-50 px-0 py-24 md:px-2">
+            <div
+                className="absolute right-3 top-3 cursor-pointer bg-transparent p-2"
+                onClick={onCopy}
+            >
+                <div
+                    className={`absolute inset-0  transform transition-all duration-300  ${hasCheckIcon ? "scale-0 opacity-0" : "scale-100 opacity-100"
+                        }`}
+                >
+                    <CopyIcon className="text-neutral-600" />
+                </div>
+                <div
+                    className={`absolute inset-0 transform transition-all duration-300  ${hasCheckIcon ? "scale-100 opacity-100" : "scale-0 opacity-0"
+                        }`}
+                >
+                    <CheckIcon className="text-neutral-600" />
+                </div>
+            </div>
+            <div ref={parentRef}  >
+            </div>
         </div>
-        <div
-          className={`absolute inset-0 transform transition-all duration-300  ${
-            hasCheckIcon ? "scale-100 opacity-100" : "scale-0 opacity-0"
-          }`}
-        >
-          <CheckIcon className="text-neutral-600" />
-        </div>
-      </div>
-      {children}
-    </div>
-  );
+    );
 };
